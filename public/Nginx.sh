963e475cefce804485ab95a15ba3059198ea360a
Murtadha Marzouq <mmarzou0@gmail.com>
2023-01-20 07:36:57 -0500

    😇

---
+++ public/Nginx.sh
@@ -0,0 +1,55 @@
+echo "Installing NGINX"
+ yum install epel-release yum-utils -y
+ yum install nginx -y
+ 
+ sleep 2 
+ systemctl start nginx
+ systemctl enable nginx
+ yum install firewalld -y
+ 
+ systemctl start firewalld
+ systemctl enable firewalld
+
+
+
+ firewall-cmd --permanent --zone=public --add-service=http
+ firewall-cmd --permanent --zone=public --add-service=https
+ firewall-cmd --zone=public --add-port=3000/tcp --permanent
+ firewall-cmd --zone=public --add-port=3001/tcp --permanent
+ firewall-cmd --reload
+ cd /usr/share/nginx/html
+
+cat > test.html << EOL
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Data</title>
+</head>
+<body>
+
+    <h1>Testing your server</h1>
+    <hr>
+    <p>
+        Go to Chrome Dev Tools (F12) and look at your Console.<br>
+        Also check your Network tab.
+    </p>
+
+    <script>
+
+        fetch('http://YOU-IP-ADDRESS:3000/json')
+            .then(response => response.json())
+            .then(data => console.log(data));
+
+        fetch('http://YOU-IP-ADDRESS:3000/text')
+            .then(response => response.text())
+            .then(data => console.log(data));
+
+    </script>
+
+</body>
+</html>
+
+
+EOL
